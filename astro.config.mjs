1b0d49987b64d83a6a2181f1ae148d9c0c7c72c7
nadinadiaz <nadinadiaz@gmail.com>
2026-02-12 19:58:55 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nadinadiaz.github.io',  
+  site: 'https://nadinadiaz.github.io',
   base: '/', 
- // adapter: edgeoneAdapter(),
   output: 'static',
   integrations: [react()]
 });
