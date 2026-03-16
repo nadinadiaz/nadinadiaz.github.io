50fefc0c788e25a74eb02bf9e58f29f7c3d77341
nadinadiaz <nadinadiaz@gmail.com>
2026-03-16 14:27:50 -0300

    Update Projects.tsx

--- src/components/portfolio/Projects.tsx
+++ src/components/portfolio/Projects.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { ExternalLink, Github, Lock, FileText } from "lucide-react";
 import capstoneBellabeat from "@/assets/resumen4.png";
-import capstoneCyclistic from "@/assets/bicis.png";
+import capstoneCyclistic from "@/assets/bicis2.png";
 
 const projects = [
   {
