a8591fd0f9e4dd524f1775e31a00ff8f6fb71533
nadinadiaz <nadinadiaz@gmail.com>
2026-03-30 13:43:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
